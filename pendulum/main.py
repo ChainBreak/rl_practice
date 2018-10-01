e276ddd4e14bec413abb29ad4bee57168934743d
Thomas Rowntree <thomas.james.rowntree@gmail.com>
2018-10-01 22:54:22 +0930

    Tidy up

--- pendulum/main.py
+++ pendulum/main.py
@@ -12,11 +12,6 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from smooth_noise import SmoothNoise
-from tensor_env import TensorEnv
-
-
-
-
 
 class RL_Trainer():
 
@@ -25,7 +20,7 @@ class RL_Trainer():
         self.num_steps = 1000000
         self.reset_interval = 100
         self.update_interval = 1
-        self.batch_size = 32
+        self.batch_size = 100
         self.replay_buffer_length = 1000
 
         self.replay_buffer = []
@@ -56,9 +51,8 @@ class RL_Trainer():
                 target_param += alpha * (new_param - target_param)
 
     def env_reset(self):
-        # self.env.state = np.array([0, 0])
-        # self.state_tensor = torch.tensor(self.env._get_obs()).float()
         state = self.env.reset()
+        #scale the states between [0,1]
         state /= np.array([1,1,self.env.max_speed])
         self.state_tensor = torch.tensor(state).float()
 
@@ -67,10 +61,9 @@ class RL_Trainer():
             #get action from actor for the current state
             action_tensor = self.actor(self.state_tensor)
 
-            #add noise to action tensor
-            noise = (1.0 - float(self.i_step)/1000000.0) * self.noise.sample()
+            #add noise to action tensor. Reduce the noise to zero at 100000 steps
+            noise = max((1.0 - float(self.i_step)/100000.0),0) * self.noise.sample()
 
-            # if not self.draw_env:
             action_tensor += noise
 
             #interact with the environment to get next state and reward
@@ -104,31 +97,20 @@ class RL_Trainer():
 
     def update_models(self):
         if len(self.replay_buffer) > self.batch_size:
-            #limit the size of the replay replay
+            #limit the size of the replay buffer
             over_size = len(self.replay_buffer) - self.replay_buffer_length
             if over_size > 0:
                 del self.replay_buffer[:over_size]
 
-
             #take a batch sample from the replay buffer
             batch_list = random.sample(self.replay_buffer, self.batch_size)
             state_batch, action_batch, reward_batch, next_state_batch = zip(*batch_list)
-            # print("\n\n")
-            # print("State",state_batch)
-            # print("Action",action_batch)
-            # print("Reward",reward_batch)
-            # print("Next State",next_state_batch)
 
             #stack the batch into tensors
             state_batch_tensor = torch.stack(state_batch)
             action_batch_tensor = torch.stack(action_batch)
             reward_batch_tensor = torch.stack(reward_batch)
             next_state_batch_tensor = torch.stack(next_state_batch)
-
-            # print("State",state_batch_tensor)
-            # print("Action",action_batch_tensor)
-            # print("Reward",reward_batch_tensor)
-            # print("Next State",next_state_batch_tensor)
 
             #Calculate target Q for updating critic
             with torch.no_grad():
@@ -146,15 +128,8 @@ class RL_Trainer():
             self.critic_optimizer.zero_grad()
             q_batch_tensor = self.critic(state_batch_tensor,action_batch_tensor)
             critic_loss = self.critic_criterion(q_batch_tensor,target_q_batch_tensor)
-            # print(float(critic_loss.data))
             critic_loss.backward()
             self.critic_optimizer.step()
-
-            # if self.draw_env:
-            #     print("Critic ############################################################")
-            #     for p in self.critic.parameters():
-            #         print(p.grad)
-
 
             #Update Actor
             self.actor_optimizer.zero_grad()
@@ -166,12 +141,7 @@ class RL_Trainer():
             reward_prediction_mean.backward()
             self.actor_optimizer.step()
 
-            # if self.draw_env:
-            #     print("Actor ############################################################")
-            #     for p in self.actor.parameters():
-            #         print(p.grad)
-
-
+            #slowly update the target networks
             self.update_target(self.actor_target , self.actor, 0.001)
             self.update_target(self.critic_target , self.critic, 0.001)
 
@@ -204,75 +174,12 @@ class RL_Trainer():
             self.env_step()
 
             #update the actor critic models every update interval
-            if self.i_step % self.update_interval == 0:
-                self.update_models()
+            # # if self.i_step % self.update_interval == 0:
+            # for i in range(3):
+            self.update_models()
 
 
 if __name__ == "__main__":
     print("Hello There")
     rlt = RL_Trainer()
     rlt.train_loop()
-    # for i_step in range(num_steps):
-    #
-    #
-    #     avg_reward = 0
-    #
-    #     # for i_step in range(num_steps):
-    #
-    #         # if i_episode > (num_episodes - 5):
-    #         #     env.render()
-    #     avg_reward /= i_step
-    #     avg_reward_list.append(avg_reward)
-    #     print(avg_reward)
-    #
-    #     #propegate reward backward
-    #     plt.plot(reward_list)
-    #     future_reward = reward_list[-1]
-    #     for i in range(len(reward_list)-2,-1,-1):
-    #         reward_list[i] += discount_factor * (future_reward - reward_list[i])
-    #         future_reward = reward_list[i]
-    #     plt.plot(reward_list)
-    #     plt.show()
-    #
-    #     # for a in reward_list:
-    #     #     print(type(a))
-    #     #     print(a.dtype)
-    #     #     print(a.shape)
-    #
-    #
-    #     #turn lists into tensor batches
-    #     state_tensor = torch.from_numpy(np.stack(state_list)).float()
-    #     action_tensor = torch.from_numpy(np.stack(action_list)).float()
-    #     state_action_tensor = torch.cat((state_tensor,action_tensor),dim=1).float()
-    #     reward_tensor = torch.from_numpy(np.stack(reward_list)).float()
-    #
-    #     # state_tensor = torch.autograd.Variable(state_tensor)
-    #     # action_tensor = torch.autograd.Variable(action_tensor)
-    #     # state_action_tensor = torch.autograd.Variable(state_action_tensor)
-    #     # reward_tensor = torch.autograd.Variable(reward_tensor)
-    #
-    #
-    #
-    #     #update Critic
-    #     critic_optimizer.zero_grad()
-    #     reward_prediction = critic(state_action_tensor)
-    #     critic_loss = critic_criterion(reward_prediction,reward_tensor)
-    #     loss_list.append(float(critic_loss.data))
-    #     # print(float(critic_loss.data))
-    #     critic_loss.backward()
-    #     critic_optimizer.step()
-    #
-    #
-    #     #update actor
-    #     if i_episode > 500:
-    #         actor_optimizer.zero_grad()
-    #         action_tensor = actor(state_tensor)
-    #         state_action_tensor = torch.cat((state_tensor,action_tensor),dim=1).float()
-    #         reward_prediction = -torch.mean(critic(state_action_tensor))
-    #         reward_prediction.backward()
-    #         actor_optimizer.step()
-    #
-    #
-    # plt.plot(loss_list)
-    # plt.plot(avg_reward_list)
-    # plt.show()
